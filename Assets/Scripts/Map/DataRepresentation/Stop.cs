80041531bfba3c118b8a086cd5e68067dfe68f6a
Krzysztof Wrona <rubikon02@gmail.com>
2024-11-24 18:17:56 +0100

    Make stop name more responsive and prepare name setter

--- Assets/Scripts/Map/DataRepresentation/Stop.cs
+++ Assets/Scripts/Map/DataRepresentation/Stop.cs
@@ -1,7 +1,14 @@
 using Map.Data;
+using TMPro;
+using UnityEngine;
 
 namespace Map.DataRepresentation {
     public class Stop : MapElement {
         public Coordinates coordinates;
+        [SerializeField] private TextMeshProUGUI nameText;
+
+        public void SetName(string stopName) {
+            nameText.text = stopName;
+        }
     }
 }
