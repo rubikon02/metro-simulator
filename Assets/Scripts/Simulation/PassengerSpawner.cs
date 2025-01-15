59b3a2880686c218a4405d8c30942c09a3742572
Krzysztof Wrona <rubikon02@gmail.com>
2025-01-15 22:09:48 +0100

    Add traffic intensity data

--- Assets/Scripts/Simulation/PassengerSpawner.cs
+++ Assets/Scripts/Simulation/PassengerSpawner.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Map;
 using Map.DataRepresentation;
@@ -8,6 +11,21 @@ using Utils;
 using Random = UnityEngine.Random;
 
 namespace Simulation {
+
+    [Serializable]
+    public class TimeTrafficAmount {
+        [Tooltip("Time in HH:MM format")]
+        public string time;
+        [Tooltip("Traffic intensity in relation to each other")]
+        public float traffic;
+    }
+    [Serializable]
+    public class DayTrafficAmount {
+        public string day;
+        public AnimationCurve trafficCurve = new();
+        public List<TimeTrafficAmount> hourValues = new();
+    }
+
     public class PassengerSpawner : MonoSingleton<PassengerSpawner> {
         [Header("Stats")]
         [SerializeField] private int spawnedCount;
@@ -20,6 +38,9 @@ namespace Simulation {
         [SerializeField] private int existingPassengerLimit = 1;
         [Header("Prefabs")]
         [SerializeField] private Passenger passengerPrefab;
+        [Header("Traffic Data")]
+        [SerializeField] private List<DayTrafficAmount> weekTrafficAmounts = new();
+
 
         public void OnPassengerRemoved(int count = 1) {
             despawnedCount += count;
@@ -27,7 +48,42 @@ namespace Simulation {
         }
 
         public void StartSpawning() {
+            LoadTrafficData("Assets/Data/DayTrafficAmounts.csv");
             StartCoroutine(SpawnPassengers());
+        }
+
+        private void LoadTrafficData(string filePath) {
+            string[] lines = File.ReadAllLines(filePath);
+            string[] headers = lines[0].Split(';').Skip(1).ToArray();
+
+            for (int i = 1; i < lines.Length; i++) {
+                string[] values = lines[i].Split(';');
+                string time = values[0];
+                for (int j = 1; j < values.Length; j++) {
+                    if (weekTrafficAmounts.Count < headers.Length) {
+                        weekTrafficAmounts.Add(new DayTrafficAmount { day = headers[j - 1] });
+                    }
+                    float trafficValue = float.Parse(values[j]);
+                    weekTrafficAmounts[j - 1].hourValues.Add(new TimeTrafficAmount { time = time, traffic = trafficValue });
+
+                    Keyframe keyframe = new(i - 1, trafficValue);
+                    weekTrafficAmounts[j - 1].trafficCurve.AddKey(keyframe);
+                }
+            }
+
+            for (int i = 0; i < weekTrafficAmounts.Count; i++) {
+                var nextDay = weekTrafficAmounts[(i + 1) % weekTrafficAmounts.Count];
+                var lastKeyframe = weekTrafficAmounts[i].trafficCurve.keys.Last();
+                var nextDayFirstKeyframe = nextDay.trafficCurve.keys.First();
+                Keyframe nextDayKeyframe = new(lastKeyframe.time + 1, nextDayFirstKeyframe.value);
+                weekTrafficAmounts[i].trafficCurve.AddKey(nextDayKeyframe);
+            }
+
+            foreach (var dayTraffic in weekTrafficAmounts) {
+                for (int i = 0; i < dayTraffic.trafficCurve.length; i++) {
+                    dayTraffic.trafficCurve.SmoothTangents(i, 0.5f);
+                }
+            }
         }
 
         private IEnumerator SpawnPassengers() {
